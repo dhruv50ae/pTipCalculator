221fa26ba053831fa893769b2fbd1a2aca299dd7
dhruv50ae <linkdp2012@gmail.com>
2022-04-04 21:01:31 +0100

    Endgame commit

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
-age = input("What is your current age? ")
-age_as_int = int(age)
-years_remaining = 90 - age_as_int
-days_remaining = years_remaining * 365
-weeks_remaining = years_remaining * 52
-months_remaining = years_remaining * 12
-message = f"You have {days_remaining} days, {weeks_remaining} weeks, and {months_remaining} months left until you hit the 90-year milestone!"
-print(message)
+print("Welcome to the tip calculator!")
+bill = float(input("What was the total bill? $"))
+tip = int(input("How much tip would you like to give? 10 12 or 15? "))
+people = int(input("How many people are splitting the bill? "))
+bill_with_tip = tip / 100 * bill + bill
+bill_per_person = bill_with_tip/people
+final_amount = round(bill_per_person, 2)
+final_amount = "{:.2f}".format(bill_per_person)
+print(f"Each person should pay ${final_amount}")
