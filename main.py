27539090dd8fe064aa7fcfaf5a99430e3675cb2d
dhruv50ae <linkdp2012@gmail.com>
2022-04-04 20:28:54 +0100

    Implemented round and f strings

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-height = input("Enter youy height in meters: ")
+height = input("Enter your height in meters: ")
 weight = input("Enter your weight in kilograms: ")
-bmi = int(weight) / float(height) ** 2
-print("Your BMI is " + str(bmi))
+bmi = int(weight) / round(float(height), 2) ** 2
+print(f"Your BMI is {bmi}")
